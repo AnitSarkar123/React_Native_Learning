3550a8084fa31c043266aba9a861a525c23c5670
as00000000 <anitsarkar04@gmail.com>
2024-07-03 21:11:26 +0530

    type_safe_components_in_react_native

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -2,14 +2,23 @@ import Colors from "@/src/constants/Colors";
 import { StyleSheet, Text, View } from "react-native";
 import { blue } from "react-native-reanimated/lib/typescript/reanimated2/Colors";
 import products from "@/assets/data/products";
+import { Product } from "@/src/types";
 // import EditScreenInfo from "@/src/components/EditScreenInfo";
 // import { Text, View } from "@/src/components/Themed";
 //const product = products[1];
 import { Image } from "react-native";
-const ProductListItem = ({ product }) => {
+const defaultPizzaimage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png";
+type ProductListItemProps = () => {
+  product: Product;
+};
+const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
     <View style={styles.container}>
-      <Image style={styles.image} source={{ uri: product.image }} />
+      <Image
+        style={styles.image}
+        source={{ uri: product.image || defaultPizzaimage }}
+      />
       <Text style={styles.title}> {product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
